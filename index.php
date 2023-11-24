2801dd7a8b862109f408812bf0fa48dac5de28f6
AminaChakirDev <amina.aitm@gmail.com>
2023-11-24 10:33:57 +0100

    deploy

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $dataTeachers = [
   'firstname' => 'Amina',
   'title' => 'Maitresse Jedi',
   'link' => 'wildersAmina.html',
-  'image' => '/assets/images/desktop/amina_small.png',
-  'arrow' => '/assets/images/Arrowright.png',
+  'image' => './assets/images/desktop/amina_small.png',
+  'arrow' => './assets/images/Arrowright.png',
 ],
 [
   'class' => 'clearClass',
@@ -17,8 +17,8 @@ $dataTeachers = [
   'firstname' => 'Benjamin',
   'title' => 'Chevalier Jedi',
   'link' => 'wildersBenjamin.html',
-  'image' => '/assets/images/desktop/benjamin_small.png',
-  'arrow' => '/assets/images/Arrowright.png',
+  'image' => './assets/images/desktop/benjamin_small.png',
+  'arrow' => './assets/images/Arrowright.png',
 ],
 ];
 
@@ -113,7 +113,7 @@ $dataStudents = [
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Best PHP Session Ever</title>
-    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
     <script src="https://kit.fontawesome.com/9fbc16b7ca.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" media="screen and (min-width: 420px)" href="./assets/css/home_desktop_style.css" type="text/css" />
   </head>
